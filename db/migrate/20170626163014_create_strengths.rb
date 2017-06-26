a8e2e40fee02b8ae79f7bdfc2174a738b0b0eab1
Tyler <tibsdoe14@gmail.com>
2017-06-26 14:11:47 -0600

    edited the migration to include the fields for the strength table

--- db/migrate/20170626163014_create_strengths.rb
+++ db/migrate/20170626163014_create_strengths.rb
@@ -1,7 +1,14 @@
 class CreateStrengths < ActiveRecord::Migration[5.1]
   def change
     create_table :strengths do |t|
-
+			t.integer :strength_id
+			t.string :name
+			t.text :desc
+			t.decimal :percent_cosm
+			t.decimal :percent_staff
+			t.decimal :percent_world
+			t.integer :leadership_id
+			add_foreign_key :strength, :leader_style, column: :leadership_id, primary_key: "id"
       t.timestamps
     end
   end

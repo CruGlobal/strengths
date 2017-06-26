375065db5fab805934e18570af09d8a45baed2c7
handlyrr <rachel.handley2591@gmail.com>
2017-06-26 15:58:33 -0600

    created the basic database for the leadership skills

---
+++ app/controllers/leader_styles_controller.rb
@@ -0,0 +1,2 @@
+class LeaderStylesController < ApplicationController
+end
---
+++ test/models/leader_style_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LeaderStyleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

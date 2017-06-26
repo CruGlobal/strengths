6bd563b9706bac9d499f0c5e19bf9fcad414599c
MikeBran <michaelbranard@gmail.com>
2017-06-26 14:03:28 -0600

    created user table, no foreign keys

--- db/migrate/20170626162941_create_users.rb
+++ db/migrate/20170626162941_create_users.rb
@@ -1,7 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-
+	t.string :fname
+	t.string :lname
+	t.string :phone_number
+	t.integer :one
+	t.integer :two
+	t.integer :three
+	t.integer :four
+	t.integer :five
+	add_foreign_key :users, :strengths, column: :one, primary_key: ("id")
+	add_foreign_key :users, :strengths, column: :two, primary_key: ("id")
+	add_foreign_key :users, :strengths, column: :three, primary_key: ("id")
+	add_foreign_key :users, :strengths, column: :four, primary_key: ("id")
+	add_foreign_key :users, :strengths, column: :five, primary_key: ("id")
       t.timestamps
     end
   end

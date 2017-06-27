3b1e50ac9bc6d1cdd54c83c8e8b27371982067b5
MikeBran <michaelbranard@gmail.com>
2017-06-27 10:34:06 -0600

    deleted unnesesary table and added correct columns to correct table

--- db/migrate/20170626221039_new_session_table_change.rb
+++ db/migrate/20170626221039_new_session_table_change.rb
@@ -2,9 +2,9 @@ class NewSessionTableChange < ActiveRecord::Migration[5.1]
   def change
   drop_table :sessions
   create_table :access_log do |t|
-	t.integer :user
-	t.string :reason_for_access
-	  t.timestamps
+		t.integer :user
+		t.string :reason_for_access
+		t.timestamps
   end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626222246) do
-
-  create_table "access_log", force: :cascade do |t|
-    t.integer "user"
-    t.string "reason_for_access"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170627155554) do
 
   create_table "access_logs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user"
+    t.string "reason_for_access"
   end
 
   create_table "users", force: :cascade do |t|
